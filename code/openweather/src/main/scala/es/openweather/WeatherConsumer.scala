575100172f6bf7d3bde1b92076bc280166fbe364
shorsh <goldman.jorge@gmail.com>
2017-12-08 15:05:15 -0300

    Join Cities

--- code/openweather/src/main/scala/es/openweather/WeatherConsumer.scala
+++ code/openweather/src/main/scala/es/openweather/WeatherConsumer.scala
@@ -1,7 +1,7 @@
 package es.openweather
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.{OutputMode, ProcessingTime}
+import org.apache.spark.sql.streaming.{ OutputMode, ProcessingTime }
 import org.apache.spark.sql.types._
 
 object WeatherConsumer extends App {
@@ -40,11 +40,11 @@ object WeatherConsumer extends App {
       .add("humidity", IntegerType, nullable = false)
       .add("temp_max", FloatType, nullable = false)
       .add("temp_min", FloatType, nullable = false), nullable = false)
-//    ESTO NO ANDA. Me base en https://stackoverflow.com/questions/39485374/how-to-create-schema-array-in-data-frame-with-spark
-//    StructField("weather", ArrayType(StructType(Array(
-//      StructField("id", LongType, nullable = false),
-//      StructField("main", LongType, nullable = false)
-//    ))), nullable = false)
+  //    ESTO NO ANDA. Me base en https://stackoverflow.com/questions/39485374/how-to-create-schema-array-in-data-frame-with-spark
+  //    StructField("weather", ArrayType(StructType(Array(
+  //      StructField("id", LongType, nullable = false),
+  //      StructField("main", LongType, nullable = false)
+  //    ))), nullable = false)
   ))
 
   import org.apache.spark.sql.functions._
@@ -53,7 +53,19 @@ object WeatherConsumer extends App {
   val jsonOptions = Map("timestampFormat" -> "yyyy-MM-dd'T'HH:mm'Z'")
   val weatherJSON = jsons.select(from_json($"value".cast("string"), schema, jsonOptions).as("values"))
   val weather = weatherJSON.select($"values.*")
+  
+  final case class CitiesCSV(
+    ID: String,
+    City: String,
+    lat: String,
+    lon: String,
+    countryCode: String)
 
+  val cities = spark.read
+    .option("header", "true")
+    .option("charset", "UTF8")
+    .csv(CITIES_FILE)
+    .as[CitiesCSV]
   weather.printSchema
 
   weather.
@@ -67,8 +79,8 @@ object WeatherConsumer extends App {
     trigger(ProcessingTime("40 seconds")).
     start()
 
-  val avgWindSpeed = weather.
-    groupBy($"id").
+  val avgWindSpeed = weather.join(cities, "ID")
+    groupBy($"City").
     agg(avg($"wind.speed"))
 
   val query = avgWindSpeed.writeStream.
