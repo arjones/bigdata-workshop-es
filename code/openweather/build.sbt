17d18fa398ca0c50a031c363d0ed4ed5606f5f77
shorsh <goldman.jorge@gmail.com>
2017-12-09 13:30:48 -0300

    DB support

--- code/openweather/build.sbt
+++ code/openweather/build.sbt
@@ -11,7 +11,7 @@ scalacOptions += "-target:jvm-1.8"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % "2.1.2" % "provided",
   "org.postgresql" % "postgresql" % "42.1.1",
-
+  "org.apache.spark" %% "spark-core" % "2.1.1",
   "org.apache.spark" %% "spark-streaming" % "2.1.2" % "provided",
   "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.1.2",
   "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.1.2",
--- code/openweather/src/main/scala/es/openweather/WeatherETL.scala
+++ code/openweather/src/main/scala/es/openweather/WeatherETL.scala
@@ -1,13 +1,17 @@
 package es.openweather
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.{ OutputMode, ProcessingTime }
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import java.util.Calendar
 
 object WeatherETL {
 
   def main(args: Array[String]): Unit = {
+    
+    //execute as
+    //spark-submit --master 'spark://master:7077'   --class "es.openweather.WeatherETL"   --total-executor-cores 2 --driver-class-path ../postgresql-42.1.4.jar  target/scala-2.11/openweather-assembly-0.1.jar 
     
     val spark = SparkSession.builder.appName("Weather:ETL").getOrCreate()
     val parquetFileDF = spark.read.parquet("/dataset/streaming-openweahter.parquet")
@@ -16,8 +20,26 @@ object WeatherETL {
 
     val DF = spark.sql("SELECT * FROM parquetFile")
 
-    DF.show
+    DatasetToPostgres.process(spark, DF)
 
   }
 
+}
+
+object DatasetToPostgres {
+
+  def process(spark: SparkSession, df: DataFrame): Unit = {
+    // Write to Postgres
+    val connectionProperties = new java.util.Properties
+    connectionProperties.put("user", "workshop")
+    connectionProperties.put("password", "w0rkzh0p")
+    connectionProperties.put("driver","org.postgresql.Driver")
+    val jdbcUrl = s"jdbc:postgresql://postgres:5432/workshop"
+
+    df.select("id","City","Temp","WindSpeed","Pressure","Humidity","Visibility","Year","Month","Day","Hour").
+      write.
+      mode(SaveMode.Append).
+      jdbc(jdbcUrl, "weather", connectionProperties)
+
+  }
 }
