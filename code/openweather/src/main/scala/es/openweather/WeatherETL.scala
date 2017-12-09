dbd847af3504518d412cd290436c7492cf58079c
shorsh <goldman.jorge@gmail.com>
2017-12-09 11:49:28 -0300

    First Version of ETL

---
+++ code/openweather/src/main/scala/es/openweather/WeatherETL.scala
@@ -0,0 +1,22 @@
+package es.openweather
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.{ OutputMode, ProcessingTime }
+import org.apache.spark.sql.types._
+import java.util.Calendar
+
+object WeatherETL {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder.appName("Weather:ETL").getOrCreate()
+    val parquetFileDF = spark.read.parquet("/dataset/streaming-openweahter.parquet")
+
+    parquetFileDF.createOrReplaceTempView("parquetFile")
+
+    val DF = spark.sql("SELECT * FROM parquetFile")
+
+    DF.show
+
+  }
+
+}
