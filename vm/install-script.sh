131395202a97faea5ae6de9c53873e7f69213f36
Gustavo Arjones <gustavo.arjones@itrust.com.ar>
2019-11-16 00:33:38 +0000

    Bump docker-compose version

--- vm/install-script.sh
+++ vm/install-script.sh
@@ -41,7 +41,7 @@ sudo groupadd docker
 sudo usermod -aG docker "${USER}"
 
 # Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 #############################################
