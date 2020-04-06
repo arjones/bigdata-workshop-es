273f77d26acf7f982bbe85c13aeb25d3cabde633
Gustavo Arjones <gustavo.arjones@ank.app>
2020-04-06 19:15:46 +0000

    Adding Docker install script

---
+++ vm/install-docker.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# SSHD
+sudo apt-get update
+
+# Docker
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo groupadd docker
+sudo usermod -aG docker "${USER}"
+
+# Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Cleanup
+sudo apt-get -y autoremove
+
+
+#############################################
+#
+# Course Material
+cd ~
+git clone https://github.com/arjones/bigdata-workshop-es.git
+
+cd bigdata-workshop-es
+# docker-compose pull
